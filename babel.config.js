8d836a59cb2162c0e81773b61c665e0562bb8369
Martin Wedvich <wedvich@gmail.com>
2019-06-02 22:27:08 +0200

    Improve Babel setup

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,16 @@ module.exports = api => {
 
   const presets = [
     '@babel/typescript',
-    ['@babel/env', {
-      corejs: 3,
-      useBuiltIns: 'entry',
-    }],
+    '@babel/env',
     '@babel/react',
   ];
 
-  const plugins = [];
+  const plugins = [
+    ['@babel/plugin-transform-runtime', {
+      corejs: 3,
+    }],
+    'babel-plugin-redux-saga',
+  ];
 
   if (process.env.NODE_ENV !== 'development') {
     plugins.push('react-hot-loader/babel');
