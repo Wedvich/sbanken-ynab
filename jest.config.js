d93bbb4d551b26abb8b4eb82a1b893b4ffc421a3
Martin Wedvich <wedvich@gmail.com>
2020-01-30 12:41:07 +0100

    Remove Express dependencies and update Jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   browser: true,
   collectCoverageFrom: ['src/**/*.*'],
   coverageReporters: ['lcov', 'cobertura'],
+  modulePathIgnorePatterns: ['<rootDir>/bin'],
   reporters: ['default', 'jest-junit'],
   testRunner: 'jest-circus/runner',
 };
