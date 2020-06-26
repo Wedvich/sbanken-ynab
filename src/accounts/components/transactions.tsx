869369709029cda33883692a00a7c6e657a5d8c2
Filip Hansen <hanfil@outlook.com>
2020-06-26 12:14:01 +0200

    Fixing str error

--- src/accounts/components/transactions.tsx
+++ src/accounts/components/transactions.tsx
@@ -48,7 +48,7 @@ const Transactions = () => {
           {accountTransactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.source}</td>
-              <td className="date">{transaction.payee[0] == "*" ? (new Date().getFullYear())+"-"+str.split(" ")[1].split(".").reverse().join("-")}</td>
+              <td className="date">{transaction.payee[0] == "*" ? (new Date().getFullYear())+"-"+transaction.payee.split(" ")[1].split(".").reverse().join("-")}</td>
               <td className="date">{formatDate(transaction.date)}</td>
               <td>{transaction.payee[0] == "*" ? transaction.payee.split(" ").slice(4,-2).join(" ") : transaction.payee}</td>
               <td>{transaction.payee}</td>
